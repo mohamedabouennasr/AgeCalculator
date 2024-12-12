283d69921192223d479eb0b41c5805c7ff791926
Mohamed Abouennasr <devmohamedabouennasr@gmail.com>
2024-12-12 12:36:52 +0100

    Edit index.html, and script.js

--- script.js
+++ script.js
@@ -9,30 +9,56 @@ const yearsEl = document.getElementById("years");
 const monthsEl = document.getElementById("months");
 const daysEl = document.getElementById("days");
 
-const currentYear = new Date();
-yearEl.attributes["max"].value = currentYear.getFullYear();
+const isLeapYear = function (year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+    ? true
+    : false;
+};
+
+const currentDate = new Date();
+yearEl.attributes["max"].value = currentDate.getFullYear();
+
+monthEl.addEventListener("change", function () {
+  if (Number(monthEl.value) === 2)
+    dayEl.attributes["max"].value = isLeapYear(Number(yearEl.value)) ? 29 : 28;
+  else if (
+    Number(monthEl.value) === 4 ||
+    Number(monthEl.value) === 6 ||
+    Number(monthEl.value) === 9 ||
+    Number(monthEl.value) === 11
+  )
+    dayEl.attributes["max"].value = 30;
+  else dayEl.attributes["max"].value = 31;
+});
+
+yearEl.addEventListener("change", function () {
+  if (Number(monthEl.value) === 2)
+    dayEl.attributes["max"].value = isLeapYear(Number(yearEl.value)) ? 29 : 28;
+});
 
 const userBirthDate = {
-  day: 0,
   month: 0,
+  day: 0,
   year: 0,
 };
 
 const userAge = {
-  days: 0,
-  months: 0,
   years: 0,
+  months: 0,
+  days: 0,
 };
 
-const calculateUserAge = function () {};
-
-calculateUserAge();
+const calculateUserAge = function () {
+  const currentDay = currentDate.getUTCDate();
+  const currentMonth = currentDate.getMonth() + 1;
+};
 
 const extractUserBirthDateAndDisplayUserAge = function () {
   event.preventDefault();
-  userBirthDate.day = dayEl.value;
   userBirthDate.month = monthEl.value;
+  userBirthDate.day = dayEl.value;
   userBirthDate.year = yearEl.value;
+  calculateUserAge();
   yearsEl.textContent = userAge.years;
   monthsEl.textContent = userAge.months;
   daysEl.textContent = userAge.days;
